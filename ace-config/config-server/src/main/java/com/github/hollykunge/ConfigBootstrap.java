f7a97218b499e870f668c5f598e3f42a35db3cb4
LiamShaw <Liam_Shaw@163.com>
2019-05-10 13:52:32 +0800

    创建config统一配置中心服务

--- ace-config/config-server/src/main/java/com/github/hollykunge/ConfigBootstrap.java
+++ ace-config/config-server/src/main/java/com/github/hollykunge/ConfigBootstrap.java
@@ -1,19 +1,23 @@
-package com.github.hollykunge.security.center;
+package com.github.hollykunge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 /**
  * ${DESCRIPTION}
  *
- * @author 协同设计小组
- * @create 2017-05-25 12:44
+ * @author 邵东旭
+ * @create 2019/05/08 15:00
  */
-@EnableEurekaServer  //启动一个服务注册中心提供给其他应用进行对话
+  //启动一个服务注册中心提供给其他应用进行对话
 @SpringBootApplication
-public class CenterBootstrap {
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigBootstrap {
     public static void main(String[] args) {
-        SpringApplication.run(CenterBootstrap.class, args);
+        SpringApplication.run(ConfigBootstrap.class, args);
     }
 }
