fa28e2f673069a98af54383a9b531a7eb464eb14
TO-ZH <386845154@qq.com>
2019-05-14 09:49:50 +0800

    文件管理类设计 通用控制类设计

---
+++ service-chat/src/main/java/com/workhub/z/servicechat/config/fileManage.java
@@ -0,0 +1,17 @@
+package com.workhub.z.servicechat.config;
+
+/**
+*@Description: 文件控制类
+*@Author: 忠
+*@date: 2019/5/14
+*/
+public class fileManage {
+
+    // TODO: 2019/5/14 文件上传
+    // TODO: 2019/5/14 文件下载
+    // TODO: 2019/5/14 文件加密 
+    // TODO: 2019/5/14 文件删除（本地磁盘）
+    // TODO: 2019/5/14 批量文件删除（本地磁盘）
+    // TODO: 2019/5/14 文件属性获取
+
+}
