12c74fd04a488d6862fe62c22501c297a034971b
dd <hollykunge@163.com>
2019-05-16 23:21:58 +0800

    修改rolebiz 完成分页

--- ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/mapper/UserMapper.java
+++ ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/mapper/UserMapper.java
@@ -3,5 +3,8 @@ package com.github.hollykunge.security.admin.mapper;
 import com.github.hollykunge.security.admin.entity.User;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+
 public interface UserMapper extends Mapper<User> {
+    List<User> selectUsersByOrgId(String orgId);
 }
