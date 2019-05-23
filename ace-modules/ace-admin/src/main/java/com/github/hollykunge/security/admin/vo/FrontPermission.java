11fec3e49d481827abe897669523dd6ada8c55d3
hollykunge <hollykunge@163.com>
2019-05-23 15:58:47 +0800

    修改前端VO对象

--- ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/vo/FrontPermission.java
+++ ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/vo/FrontPermission.java
@@ -1,5 +1,7 @@
 package com.github.hollykunge.security.admin.vo;
 
+import lombok.Data;
+
 import java.util.List;
 
 /**
@@ -8,15 +10,22 @@ import java.util.List;
  * @author: holly
  * @since: 2019/5/9
  */
+@Data
 public class FrontPermission {
     //角色id
     private String roleId;
-    //菜单权限id
-    private String permissionId;
-    //菜单权限名称
-    private String permissionName;
-    //功能集合
-    private String actions;
+    /**
+     * permissionId->menuId
+     */
+    private String menuId;
+    /**
+     * permissionName->title
+     */
+    private String title;
+    /**
+     * actions->methods
+     */
+    private String methods;
     //暂时没用上null
     private String actionList;
     //暂时没用上null
@@ -24,59 +33,4 @@ public class FrontPermission {
 
     private List<ActionEntitySet> actionEntitySetList;
 
-    public String getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(String roleId) {
-        this.roleId = roleId;
-    }
-
-    public String getPermissionId() {
-        return permissionId;
-    }
-
-    public void setPermissionId(String permissionId) {
-        this.permissionId = permissionId;
-    }
-
-    public String getPermissionName() {
-        return permissionName;
-    }
-
-    public void setPermissionName(String permissionName) {
-        this.permissionName = permissionName;
-    }
-
-    public String getActions() {
-        return actions;
-    }
-
-    public void setActions(String actions) {
-        this.actions = actions;
-    }
-
-    public String getActionList() {
-        return actionList;
-    }
-
-    public void setActionList(String actionList) {
-        this.actionList = actionList;
-    }
-
-    public String getDataAccess() {
-        return dataAccess;
-    }
-
-    public void setDataAccess(String dataAccess) {
-        this.dataAccess = dataAccess;
-    }
-
-    public List<ActionEntitySet> getActionEntitySetList() {
-        return actionEntitySetList;
-    }
-
-    public void setActionEntitySetList(List<ActionEntitySet> actionEntitySetList) {
-        this.actionEntitySetList = actionEntitySetList;
-    }
 }
--- ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/vo/FrontUser.java
+++ ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/vo/FrontUser.java
@@ -1,15 +1,18 @@
 package com.github.hollykunge.security.admin.vo;
 
+import lombok.Data;
+
 /**
  * 业务对象：前端获取用户信息
  * @author 协同设计小组
  * @date 2017/8/22
  */
+@Data
 public class FrontUser {
     //系统内id
     private String id;
-    //身份证号
-    private String username;
+    //身份证号 username->pId
+    private String pId;
     //用户姓名
     private String name;
     //自述信息
@@ -18,95 +21,13 @@ public class FrontUser {
     private String avatar;
     //状态0离职1在职
     private Integer status;
-    //电话
-    private Integer telephone;
-    //系统内删除状态0未删除
+    //电话 telephone->mobile
+    private Integer mobile;
+    //系统内删除状态0未删除 暂无
     private Integer deleted;
     //角色
     private String roleId;
 
     private UserRole userRole;
-
-
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public Integer getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(Integer telephone) {
-        this.telephone = telephone;
-    }
-
-    public Integer getDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(Integer deleted) {
-        this.deleted = deleted;
-    }
-
-    public String getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(String roleId) {
-        this.roleId = roleId;
-    }
-
-    public UserRole getUserRole() {
-        return userRole;
-    }
-
-    public void setUserRole(UserRole userRole) {
-        this.userRole = userRole;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
 }
