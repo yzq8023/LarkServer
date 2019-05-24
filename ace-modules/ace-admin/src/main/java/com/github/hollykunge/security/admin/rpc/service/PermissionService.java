9aa057a913ac0c33e0c72a016ac49fdbba6a044d
hollykunge <hollykunge@163.com>
2019-05-24 19:12:25 +0800

    需要修改permissionservice

--- ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/rpc/service/PermissionService.java
+++ ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/rpc/service/PermissionService.java
@@ -2,6 +2,7 @@ package com.github.hollykunge.security.admin.rpc.service;
 
 import com.github.hollykunge.security.admin.biz.ElementBiz;
 import com.github.hollykunge.security.admin.biz.MenuBiz;
+import com.github.hollykunge.security.admin.biz.RoleBiz;
 import com.github.hollykunge.security.admin.biz.UserBiz;
 import com.github.hollykunge.security.admin.constant.AdminCommonConstant;
 import com.github.hollykunge.security.admin.entity.Element;
@@ -28,10 +29,14 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * Created by 协同设计小组 on 2017/9/12.
+ *
+ * @author 协同设计小组
+ * @date 2017/9/12
  */
 @Service
 public class PermissionService {
+    @Autowired
+    private RoleBiz roleBiz;
     @Autowired
     private UserBiz userBiz;
     @Autowired
@@ -93,13 +98,13 @@ public class PermissionService {
         }
     }
 
-    public List<PermissionInfo> getPermissionByUsername(String username) {
-        User user = userBiz.getUserByUsername(username);
+    public List<PermissionInfo> getPermissionByUserId(String userId) {
+        User user = userBiz.getUserByUsername(userId);
         List<Menu> menus = menuBiz.getUserAuthorityMenuByUserId(user.getId());
         List<PermissionInfo> result = new ArrayList<PermissionInfo>();
         PermissionInfo info = null;
         menu2permission(menus, result);
-        List<Element> elements = elementBiz.getAuthorityElementByUserId(user.getId() + "");
+        List<Element> elements = elementBiz.getAuthorityElementByUserId(user.getId());
         element2permission(result, elements);
         return result;
     }
@@ -155,11 +160,11 @@ public class PermissionService {
         return getMenuTree(menus,AdminCommonConstant.ROOT);
     }
 
-    public UserRole getUserRoleByUserId(String username) {
-        Role role = getRoleByUserId(username);
+    public UserRole getUserRoleByUserId(String userId) {
+        Role role = roleBiz.getRoleByUserId(userId);
         UserRole userRole = new UserRole();
         BeanUtils.copyProperties(role, userRole);
-        List<PermissionInfo> permissionInfoList = this.getPermissionByUsername(username);
+        List<PermissionInfo> permissionInfoList = this.getPermissionByUserId(userId);
         userRole.setPermissionInfos(permissionInfoList);
         return userRole;
     }
