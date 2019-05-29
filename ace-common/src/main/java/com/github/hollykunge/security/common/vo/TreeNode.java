cd055b140227f528ac5c1df4a740efabb6747222
zhhongyu <18525788842@163.com>
2019-05-29 19:01:10 +0800

    tree的父类将id，parentid int类型改为string类型

--- ace-common/src/main/java/com/github/hollykunge/security/common/vo/TreeNode.java
+++ ace-common/src/main/java/com/github/hollykunge/security/common/vo/TreeNode.java
@@ -7,8 +7,8 @@ import java.util.List;
  * Created by 协同设计小组 on 2017/6/12.
  */
 public class TreeNode {
-    protected int id;
-    protected int parentId;
+    protected String id;
+    protected String parentId;
 
     public List<TreeNode> getChildren() {
         return children;
@@ -20,19 +20,19 @@ public class TreeNode {
 
     List<TreeNode> children = new ArrayList<TreeNode>();
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
-    public int getParentId() {
+    public String getParentId() {
         return parentId;
     }
 
-    public void setParentId(int parentId) {
+    public void setParentId(String parentId) {
         this.parentId = parentId;
     }
 
