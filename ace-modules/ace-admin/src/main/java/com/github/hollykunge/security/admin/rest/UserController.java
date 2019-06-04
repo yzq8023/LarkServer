fb38cf956d5406dcaddfa53f17ee3e3000678cbd
zhhongyu <18525788842@163.com>
2019-06-04 10:30:45 +0800

    将接口返回值集体改为ListRestResponse类型

--- ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/rest/UserController.java
+++ ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/rest/UserController.java
@@ -4,16 +4,13 @@ import com.github.hollykunge.security.admin.biz.UserBiz;
 import com.github.hollykunge.security.admin.entity.User;
 import com.github.hollykunge.security.admin.rpc.service.PermissionService;
 import com.github.hollykunge.security.admin.vo.FrontUser;
-import com.github.hollykunge.security.admin.vo.MenuTree;
+import com.github.hollykunge.security.common.msg.ListRestResponse;
 import com.github.hollykunge.security.common.rest.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
  * 用户相关信息接口
@@ -31,16 +28,12 @@ public class UserController extends BaseController<UserBiz,User> {
     private PermissionService permissionService;
     @RequestMapping(value = "/front/info", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<?> getUserInfo( String token, HttpServletRequest request) throws Exception {
+    public ListRestResponse<?> getUserInfo(String token, HttpServletRequest request) throws Exception {
         if(StringUtils.isEmpty(token)){
             token = request.getHeader(headerName);
         }
         FrontUser userInfo = permissionService.getUserInfo(token);
-        if(userInfo==null) {
-            return ResponseEntity.status(401).body(false);
-        } else {
-            return ResponseEntity.ok(userInfo);
-        }
+        return new ListRestResponse("",0,userInfo);
     }
 
 //    @RequestMapping(value = "/front/menus", method = RequestMethod.GET)
