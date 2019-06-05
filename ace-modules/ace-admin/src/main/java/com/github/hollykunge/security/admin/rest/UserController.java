8960f3da6758febb319ab8202496fd7afe0b5bc3
zhhongyu <18525788842@163.com>
2019-06-05 16:44:06 +0800

    由于新增用户接口需要携带userid，故而重新定义新增添加用户接口      * ps：由于添加用户的时候需要给用户角色，故而单独提供一个      * 添加用户的接口，该接口携带返回值

--- ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/rest/UserController.java
+++ ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/rest/UserController.java
@@ -3,10 +3,12 @@ package com.github.hollykunge.security.admin.rest;
 import com.github.hollykunge.security.admin.biz.UserBiz;
 import com.github.hollykunge.security.admin.entity.User;
 import com.github.hollykunge.security.admin.rpc.service.PermissionService;
+import com.github.hollykunge.security.admin.vo.AdminUser;
 import com.github.hollykunge.security.admin.vo.FrontUser;
 import com.github.hollykunge.security.common.msg.ListRestResponse;
 import com.github.hollykunge.security.common.msg.ObjectRestResponse;
 import com.github.hollykunge.security.common.rest.BaseController;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.StringUtils;
@@ -48,6 +50,23 @@ public class UserController extends BaseController<UserBiz,User> {
     public ObjectRestResponse modifyUserRoles(@RequestParam("userId")String userId, @RequestParam("roles")String roles){
         baseBiz.modifyRoles(userId,roles);
         return new ObjectRestResponse().rel(true).msg("");
+    }
+
+    /**
+     * 添加用户接口
+     * ps：由于添加用户的时候需要给用户角色，故而单独提供一个
+     * 添加用户的接口，该接口携带返回值
+     * @param entity 用户实体
+     * @return 用户id
+     */
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
+    @ResponseBody
+    public ObjectRestResponse<AdminUser> addUser(@RequestBody User entity) {
+        User user = baseBiz.addUser(entity);
+        //未防止暴露密码使用AdminUser携带id返回前端
+        AdminUser adminUser = new AdminUser();
+        BeanUtils.copyProperties(user,adminUser);
+        return new ObjectRestResponse<AdminUser>().data(adminUser);
     }
 
 //    @RequestMapping(value = "/front/menus", method = RequestMethod.GET)
