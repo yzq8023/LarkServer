3435d17a86423065802050d4ff0e2275443fbf6c
zhhongyu <18525788842@163.com>
2019-06-05 12:29:40 +0800

    将接口返回值集体改为ListRestResponse类型

--- ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/rest/RoleController.java
+++ ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/rest/RoleController.java
@@ -8,6 +8,7 @@ import com.github.hollykunge.security.admin.vo.AdminPermission;
 import com.github.hollykunge.security.admin.vo.AdminUser;
 import com.github.hollykunge.security.admin.vo.RoleTree;
 import com.github.hollykunge.security.admin.vo.UserRole;
+import com.github.hollykunge.security.common.msg.ListRestResponse;
 import com.github.hollykunge.security.common.msg.ObjectRestResponse;
 import com.github.hollykunge.security.common.msg.TableResultResponse;
 import com.github.hollykunge.security.common.rest.BaseController;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author 协同设计小组
@@ -39,8 +41,9 @@ public class RoleController extends BaseController<RoleBiz, Role> {
      */
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     @ResponseBody
-    public ObjectRestResponse<List<AdminUser>> getUsers(@RequestParam("roleId") String id) {
-        return new ObjectRestResponse<List<AdminUser>>().data(baseBiz.getRoleUsers(Integer.parseInt(id))).rel(true);
+    public ListRestResponse<List<AdminUser>> getUsers(@RequestParam("id") String id) {
+        List<AdminUser> roleUsers = baseBiz.getRoleUsers(Integer.parseInt(id));
+        return new ListRestResponse<>("",roleUsers.size(),roleUsers);
     }
 
     /**
@@ -51,20 +54,20 @@ public class RoleController extends BaseController<RoleBiz, Role> {
      */
     @RequestMapping(value = "/user", method = RequestMethod.PUT)
     @ResponseBody
-    public ObjectRestResponse modifyUsers(@RequestParam("roleId") String id, @RequestParam("users") String users) {
+    public ObjectRestResponse modifyUsers(@RequestParam("id") String id, @RequestParam("users") String users) {
         baseBiz.modifyRoleUsers(id, users);
-        return new ObjectRestResponse().rel(true);
+        return new ObjectRestResponse().rel(true).msg("修改成功");
     }
 
     /**
      * 批量修改角色菜单
-     *
-     * @param id        角色id
-     * @param permissionList 被勾选的权限Element和Element所对应的菜单信息
+     * @param permissionMap  包含 角色id和权限list
      */
     @RequestMapping(value = "/permission", method = RequestMethod.PUT)
     @ResponseBody
-    public ObjectRestResponse modifyMenuAuthority(@RequestParam("roleId") String id, @RequestBody List<AdminPermission> permissionList) {
+    public ObjectRestResponse modifyMenuAuthority(@RequestBody Map<String,Object> permissionMap) {
+        String id =(String)permissionMap.get("id");
+        List<AdminPermission> permissionList = (List<AdminPermission>)permissionMap.get("permissionList");
         baseBiz.modifyAuthorityMenu(id, permissionList);
         return new ObjectRestResponse().rel(true);
     }
@@ -76,8 +79,9 @@ public class RoleController extends BaseController<RoleBiz, Role> {
      */
     @RequestMapping(value = "/permission", method = RequestMethod.GET)
     @ResponseBody
-    public ObjectRestResponse<List<AdminPermission>> getMenuAuthority(@RequestParam("roleId") String id) {
-        return new ObjectRestResponse().data(baseBiz.getAuthorityMenu(id)).rel(true);
+    public ListRestResponse<List<AdminPermission>> getMenuAuthority(@RequestParam("id") String id) {
+        List<AdminPermission> authorityMenu = baseBiz.getAuthorityMenu(id);
+        return new ListRestResponse("",authorityMenu.size(),authorityMenu);
     }
 
 
@@ -86,11 +90,12 @@ public class RoleController extends BaseController<RoleBiz, Role> {
      */
     @RequestMapping(value = "/tree", method = RequestMethod.GET)
     @ResponseBody
-    public List<RoleTree> tree(@RequestParam("parentTreeId") String parentTreeId) {
+    public ListRestResponse<List<RoleTree>> tree(@RequestParam("parentTreeId") String parentTreeId) {
         if(StringUtils.isEmpty(parentTreeId)){
             parentTreeId = AdminCommonConstant.ROOT;
         }
-        return getTree(baseBiz.selectListAll(), parentTreeId);
+        List<RoleTree> tree = getTree(baseBiz.selectListAll(), parentTreeId);
+        return new ListRestResponse<>("",tree.size(),tree);
     }
 
     private List<RoleTree> getTree(List<Role> roles, String root) {
