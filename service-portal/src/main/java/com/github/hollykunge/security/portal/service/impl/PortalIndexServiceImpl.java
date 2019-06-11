9b7ab059607efd2d24aa6e710de787de4e9b648c
zhhongyu <18525788842@163.com>
2019-06-11 17:29:09 +0800

    删除测试用的service业务层

--- service-portal/src/main/java/com/github/hollykunge/security/portal/service/impl/PortalIndexServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.github.hollykunge.security.portal.service.impl;
-
-import com.github.hollykunge.security.portal.service.PortalIndexService;
-
-public class PortalIndexServiceImpl extends PortalIndexService {
-}
