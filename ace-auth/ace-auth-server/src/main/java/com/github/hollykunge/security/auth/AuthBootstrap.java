d27e13975956c47d3b20d015e3c7c8d7e8cd1463
zhhongyu <18525788842@163.com>
2019-06-28 10:38:28 +0800

    auth服务使用common中的DruidConfig文件

--- ace-auth/ace-auth-server/src/main/java/com/github/hollykunge/security/auth/AuthBootstrap.java
+++ ace-auth/ace-auth-server/src/main/java/com/github/hollykunge/security/auth/AuthBootstrap.java
@@ -1,14 +1,17 @@
 package com.github.hollykunge.security.auth;
 
 
+import com.github.hollykunge.security.common.config.DruidConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 /**
  * Created by 协同设计小组 on 2017/6/2.
@@ -17,7 +20,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableEurekaClient
 @EnableFeignClients
 @MapperScan("com.github.hollykunge.security.auth.mapper")
-@ComponentScan(basePackages = "com.github.hollykunge.security")
+@Import(DruidConfig.class)
 //@RemoteApplicationEventScan(basePackages = "com.github.hollykunge.security.auth.common.event")
 public class AuthBootstrap {
     public static void main(String[] args) {
