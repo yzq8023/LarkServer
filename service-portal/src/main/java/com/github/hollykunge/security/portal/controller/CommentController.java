89b29e369a02f4b78182939ac2e59047d5a47c23
zhhongyu <18525788842@163.com>
2019-07-01 16:13:28 +0800

    增加根据问题id查询评论接口

--- service-portal/src/main/java/com/github/hollykunge/security/portal/controller/CommentController.java
+++ service-portal/src/main/java/com/github/hollykunge/security/portal/controller/CommentController.java
@@ -1,8 +1,15 @@
 package com.github.hollykunge.security.portal.controller;
 
+import com.github.hollykunge.security.common.msg.ListRestResponse;
 import com.github.hollykunge.security.common.rest.BaseController;
 import com.github.hollykunge.security.entity.Comment;
 import com.github.hollykunge.security.portal.service.CommentService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @ClassName CommentController
@@ -11,5 +18,18 @@ import com.github.hollykunge.security.portal.service.CommentService;
  * @Date 2019/6/30 13:27
  * @Version 1.0
  **/
+@RestController
+@RequestMapping("comment")
 public class CommentController extends BaseController<CommentService, Comment> {
+    /**
+     * 根据问题反馈id获取对应的评论列表
+     * @param feedBackId 问题反馈id
+     * @return
+     */
+    @RequestMapping("feedBackComments")
+    @ResponseBody
+    public ListRestResponse<List<Comment>> feedBackComments(@RequestParam String feedBackId){
+        List<Comment> comments = baseBiz.feedBackComments(feedBackId);
+        return new ListRestResponse<List<Comment>>("",comments.size(),comments);
+    }
 }
