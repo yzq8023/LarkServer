ef3adb6a3e89251d14c54d6cdddcf7f5525a2a4d
TO-ZH <386845154@qq.com>
2019-07-02 18:05:46 +0800

    增加 登录 接口 升级websocket 修改配置文件

--- service-chat/src/main/java/com/workhub/z/servicechat/feign/IValidateService.java
+++ service-chat/src/main/java/com/workhub/z/servicechat/feign/IValidateService.java
@@ -2,6 +2,7 @@ package com.workhub.z.servicechat.feign;
 
 import com.github.hollykunge.security.common.msg.ObjectRestResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 *@date: 2019/4/11
 */
 @FeignClient(value = "ace-auth")
+@Repository
 public interface IValidateService {
     void validate(String token) throws Exception;
 
-    @RequestMapping(value = "/token/generate", method = RequestMethod.GET)
-    public ObjectRestResponse<?> generate(@RequestParam("username")String username,@RequestParam("password") String password) throws Exception;
+    @RequestMapping(value = "/api/token/generate", method = RequestMethod.GET)
+    public ObjectRestResponse generate(@RequestParam("username") String username,@RequestParam("password") String password) throws Exception;
 }
