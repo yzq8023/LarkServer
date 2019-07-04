291f7e8ce7d2c384e15b9ad05799f7d20c34a14b
zhuqz <785531296@qq.com>
2019-07-04 09:51:36 +0800

    上传文件字段调整，增加上传人姓名 无用文件定时删除

--- service-chat/src/main/java/com/workhub/z/servicechat/service/impl/JobServiceImpl.java
+++ service-chat/src/main/java/com/workhub/z/servicechat/service/impl/JobServiceImpl.java
@@ -1,12 +1,17 @@
 package com.workhub.z.servicechat.service.impl;
 
 
+import com.workhub.z.servicechat.config.common;
+import com.workhub.z.servicechat.config.fileManage;
 import com.workhub.z.servicechat.dao.JobDao;
 import com.workhub.z.servicechat.service.JobService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,6 +22,7 @@ import java.util.Map;
  */
 @Service("jobServiceImpl")
 public class JobServiceImpl implements JobService {
+    private static Logger log = LoggerFactory.getLogger(JobServiceImpl.class);
     @Resource
     private JobDao jobDao;
     public String backupHisMsg() throws Exception{
@@ -31,5 +37,28 @@ public class JobServiceImpl implements JobService {
             res="-1";
         }
         return  res;
+    }
+    /**
+     * 定时任务-处理无用附件
+     *
+     */
+    public void dealUnUsedFileList() throws Exception{
+        try {
+            List<String> filePathList=this.jobDao.getUnUsedFileList();
+            this.jobDao.delUnUsedFileList();
+            for(String path:filePathList){
+                try {
+                    fileManage.delUploadFile(path);
+                } catch (Exception e) {
+                    throw e;
+                }
+
+            }
+            log.info("定时任务执行成功，共删除无用附件"+filePathList.size()+"个");
+        }catch (Exception e){
+            log.error("定时任务删除无用附件出错：");
+            log.error(common.getExceptionMessage(e));
+        }
+
     }
 }
