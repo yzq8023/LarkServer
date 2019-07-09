71e621bc2e5e9dacc55816d3b5bafc772e314048
hollykunge <hollykunge@163.com>
2019-07-09 16:01:16 +0800

    在notice和feedback中重写了分页

--- service-portal/src/main/java/com/github/hollykunge/security/portal/service/FeedbackService.java
+++ service-portal/src/main/java/com/github/hollykunge/security/portal/service/FeedbackService.java
@@ -1,10 +1,19 @@
 package com.github.hollykunge.security.portal.service;
 
 import com.github.hollykunge.security.common.biz.BaseBiz;
+import com.github.hollykunge.security.common.msg.TableResultResponse;
+import com.github.hollykunge.security.common.util.Query;
 import com.github.hollykunge.security.entity.Feedback;
 import com.github.hollykunge.security.mapper.FeedbackMapper;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName FeedBackService
@@ -19,5 +28,20 @@ public class FeedbackService extends BaseBiz<FeedbackMapper, Feedback> {
     @Override
     protected String getPageName() {
         return null;
+    }
+
+    @Override
+    public TableResultResponse<Feedback> selectByQuery(Query query) {
+        Class<Feedback> clazz = (Class<Feedback>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[1];
+        Example example = new Example(clazz);
+        if(query.entrySet().size()>0) {
+            Example.Criteria criteria = example.createCriteria();
+            for (Map.Entry<String, Object> entry : query.entrySet()) {
+                criteria.andLike(entry.getKey(), "%" + entry.getValue().toString() + "%");
+            }
+        }
+        Page<Object> result = PageHelper.startPage(query.getPageNo(), query.getPageSize());
+        List<Feedback> list = mapper.selectByExample(example);
+        return new TableResultResponse<Feedback>(result.getPageSize(), result.getPageNum() ,list.size()/10+1, list.size(), list);
     }
 }
