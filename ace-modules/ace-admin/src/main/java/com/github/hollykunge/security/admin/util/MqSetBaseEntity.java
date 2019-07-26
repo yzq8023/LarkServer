e23bd6d14476067b2a32ff16f2e869f4a484f3f1
zhhongyu <18525788842@163.com>
2019-07-26 19:12:01 +0800

    mq使用基类赋值，改一下反射方式

--- ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/util/MqSetBaseEntity.java
+++ ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/util/MqSetBaseEntity.java
@@ -1,5 +1,7 @@
 package com.github.hollykunge.security.admin.util;
 
+import com.github.hollykunge.security.common.util.UUIDUtils;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.InetAddress;
@@ -12,27 +14,29 @@ import java.util.Date;
  * @since 2019-07-23
  */
 public class MqSetBaseEntity {
-    public static <T> T setCreatData(T t) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, UnknownHostException {
-        Method setCrtTime = t.getClass().getMethod("setCrtTime");
-        Method setCrtUser = t.getClass().getMethod("setCrtUser");
-        Method setCrtName = t.getClass().getMethod("setCrtName");
-        Method setCrtHost = t.getClass().getMethod("setCrtHost");
-        setCrtTime.invoke(Date.class,new Date());
-        setCrtUser.invoke(String.class,"mq");
-        setCrtName.invoke(String.class,"mq");
-        setCrtHost.invoke(String.class,InetAddress.getLocalHost().toString());
-        return t;
+    public static <T> void setCreatData(T t) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, UnknownHostException, InstantiationException {
+        Class<?> aClass = t.getClass();
+        Method setCrtTime = aClass.getMethod("setCrtTime",Date.class);
+        Method setCrtUser = aClass.getMethod("setCrtUser",String.class);
+        Method setCrtName = aClass.getMethod("setCrtName",String.class);
+        Method setCrtHost = aClass.getMethod("setCrtHost",String.class);
+        Method setId = aClass.getMethod("setId", String.class);
+        setCrtTime.invoke(t,new Date());
+        setCrtUser.invoke(t,"mq");
+        setCrtName.invoke(t,"mq");
+        setId.invoke(t, UUIDUtils.generateShortUuid());
+        setCrtHost.invoke(t,InetAddress.getLocalHost().toString());
     }
 
     public static <T> T setUpdate(T t) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, UnknownHostException {
-        Method setCrtTime = t.getClass().getMethod("setUpdTime");
-        Method setUpdUser = t.getClass().getMethod("setUpdUser");
-        Method setUpdName = t.getClass().getMethod("setUpdName");
-        Method setUpdHost = t.getClass().getMethod("setUpdHost");
-        setCrtTime.invoke(Date.class,new Date());
-        setUpdUser.invoke(String.class,"mq");
-        setUpdName.invoke(String.class,"mq");
-        setUpdHost.invoke(String.class,InetAddress.getLocalHost().toString());
+        Method setCrtTime = t.getClass().getMethod("setUpdTime",Date.class);
+        Method setUpdUser = t.getClass().getMethod("setUpdUser",String.class);
+        Method setUpdName = t.getClass().getMethod("setUpdName",String.class);
+        Method setUpdHost = t.getClass().getMethod("setUpdHost",String.class);
+        setCrtTime.invoke(t,new Date());
+        setUpdUser.invoke(t,"mq");
+        setUpdName.invoke(t,"mq");
+        setUpdHost.invoke(t,InetAddress.getLocalHost().toString());
         return t;
     }
 }
