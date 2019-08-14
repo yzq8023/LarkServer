90484ac2ff6d0c39b0f44ed4a06f3cf3fd965a08
zhuqz <785531296@qq.com>
2019-08-14 19:30:06 +0800

    群消息审计

---
+++ service-chat/src/main/java/com/workhub/z/servicechat/VO/MessageMonitoringPrivateVo.java
@@ -0,0 +1,69 @@
+package com.workhub.z.servicechat.VO;
+
+/**
+ * author:zhuqz
+ * description:消息监控私聊
+ * date:2019/8/14 16:24
+ **/
+public class MessageMonitoringPrivateVo {
+    //发送人姓名
+    private String senderName;
+    //发送人密级
+    private String senderLevel;
+    //发送时间 yyyy-mm-dd hh:mi:ss
+    private String sendTime;
+    //接收人姓名
+    private String receiverName;
+    //消息密级
+    private String messageLevel;
+    //消息内容
+    private String messageContent;
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getSenderLevel() {
+        return senderLevel;
+    }
+
+    public void setSenderLevel(String senderLevel) {
+        this.senderLevel = senderLevel;
+    }
+
+    public String getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(String sendTime) {
+        this.sendTime = sendTime;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getMessageLevel() {
+        return messageLevel;
+    }
+
+    public void setMessageLevel(String messageLevel) {
+        this.messageLevel = messageLevel;
+    }
+
+    public String getMessageContent() {
+        return messageContent;
+    }
+
+    public void setMessageContent(String messageContent) {
+        this.messageContent = messageContent;
+    }
+}
