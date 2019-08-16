ea0e3b2e936807256c83f2d416285c1df69b335c
TO-ZH <386845154@qq.com>
2019-08-16 18:01:21 +0800

    日志bug修复

--- ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/rest/GateLogController.java
+++ ace-modules/ace-admin/src/main/java/com/github/hollykunge/security/admin/rest/GateLogController.java
@@ -60,7 +60,6 @@ public class GateLogController extends BaseController<GateLogBiz,GateLog> {
         userPidlist.add("3");
 //        Log.debug(userName+SYSTEM_USER+LOG_USER+SECURITY_USER);
         Log.debug("用户姓名" +request.getHeader("userName")+"用户身份证"+request.getHeader("dnname"));
-        System.out.println(userName+SYSTEM_USER+LOG_USER+SECURITY_USER);
         System.out.println("用户姓名" +request.getHeader("userName")+"用户身份证"+request.getHeader("dnname"));
         if (userName.equals(LOG_USER))
         {
